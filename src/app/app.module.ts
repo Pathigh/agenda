ed64f22d7ce8fe20331a2a0197f2dcdbc6920db3
pathigh <pat.venegas.l@gmail.com>
2020-08-26 17:30:02 -0400

    skin update + Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { PagesnofoundComponent } from './pages/pagesnofound/pagesnofound.compone
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { ClientsComponent } from './pages/clients/clients.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { UsersComponent } from './pages/users/users.component';
+import { ScheduleComponent } from './pages/schedule/schedule.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,12 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
     PagesnofoundComponent,
     DashboardComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    ClientsComponent,
+    CategoryComponent,
+    UsersComponent,
+    ScheduleComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule,
